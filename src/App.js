47c23d3601e12ca014259dee6350ab1bc22e1824
Yav Gz <gutierrez.alcantara.javier@gmail.com>
2023-02-11 22:36:53 -0600

    Componentes de todo machine

--- src/App.js
+++ src/App.js
@@ -1,35 +1,33 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-import './Card.css'
+import { TodoCounter } from './TodoCounter.js'
+import { TodoSearch } from './TodoSearch';
+import { TodoList } from './TodoList';
+import { TodoItem } from './TodoItem';
+import { CreateTodoButton } from './CreateTodoButton';
+
+const todos = [
+  { text: 'Cortar cebolla', completed: false}, 
+  { text: 'Terminar este curso', completed: false}, 
+];
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <React.Fragment>
+      <TodoCounter/>
 
-        <div className="card-container">
-          <img src="" alt='imagen por preparar'></img>
-          <p>
-            La imagen aun no está definida
-          </p>
-        </div>
+      <TodoSearch/>
+      <input placeholder='cebolla'/>
+      <TodoList>
+        {todos.map( todo => (
+          <TodoItem key={todo.text} text={todo.text } />
+          ) )} 
+      </TodoList>
 
-      </header>
+      <CreateTodoButton/>
 
-
-    </div>
+    </React.Fragment>
   );
 }
 
