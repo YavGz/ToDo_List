85fc43aa1facbe798649ea6be5f7fe454c8e00fe
Yav Gz <gutierrez.alcantara.javier@gmail.com>
2023-02-18 14:21:11 -0600

    Add TodoCouter CSS

--- src/TodoCounter.js
+++ src/TodoCounter.js
@@ -1,8 +1,12 @@
 import React from "react";
+import './TodoCounter.css';
 
 function TodoCounter() {
   return (
-    <h2>Has completado 2 de 3 TODOs</h2>
+    <React.Fragment>
+      <h1 className="todoCounter_title">Bienvenido de vuelta, Usuario</h1>
+      <h2 className="todoCounter">Has completado 2 de 3 TODOs</h2>
+    </React.Fragment>
   );
 };
 
