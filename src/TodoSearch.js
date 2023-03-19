3c7d8d2830802fe33296778b0708dd5e18fac857
Yav Gz <gutierrez.alcantara.javier@gmail.com>
2023-03-19 16:13:03 -0600

    Pre de la funcion para filtrado de tareas con TodoSearch

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -2,12 +2,24 @@ import React from "react";
 import "./TodoSearch.css"
 
 function TodoSearch(){
+  const [SearchValue, setSearchValue] = React.useState('');
+
+  const onSearchValueChange = (event) => {
+    console.log(event.target.value);
+    setSearchValue(event.target.value);
+  }
+
   return(
     <React.Fragment>
       <div className="todoSearch">
-        <input placeholder="Buscar ToDo" />
+        <input 
+          placeholder="Buscar ToDo" 
+          value={SearchValue}
+          onChange={onSearchValueChange}
+        />
         <span></span>
       </div>
+      <p>{SearchValue}</p>
     </React.Fragment>
   );
 }
