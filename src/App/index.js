5b242f1c7bf1eab0765a3cec2135022dea90b3a2
Yav Gz <gutierrez.alcantara.javier@gmail.com>
2023-04-07 17:16:37 -0600

    Custom Hook para LocalStorage

--- src/App/index.js
+++ src/App/index.js
@@ -10,21 +10,34 @@ import { AppUI } from './AppUI';
 //   { text: 'Montar una App para GTD', completed: false}
 // ];
 
-function App() {
-
-  const localStorageTodos = localStorage.getItem('TODOS_V1') 
-  
-  let parsedTodos;
-
-  if (!localStorageTodos) {
-    localStorage.setItem('TODOS_V1', JSON.stringify([]));
-    parsedTodos = [];
+function useLocalStorage (itemName, initialValue) {
+  const localStorageItem = localStorage.getItem(itemName) 
+    let parsedItem;
+    
+    if (!localStorageItem) {
+      localStorage.setItem(itemName, JSON.stringify(initialValue));
+    parsedItem = initialValue;
   } else {
-    parsedTodos = JSON.parse(localStorageTodos)
-  }
+    parsedItem = JSON.parse(localStorageItem)
+  } 
   
+  const [item, setItem] = React.useState(parsedItem)
   
-  const [todos, setTodos] = React.useState(parsedTodos)
+  function saveItem (newItem) {
+    const stringifiedItem = JSON.stringify(newItem);
+    localStorage.setItem(itemName, stringifiedItem);
+    setItem(newItem);
+  };
+
+  return [
+    item,
+    saveItem,
+  ]
+};
+
+function App() {  
+
+  const [todos, saveTodos] = useLocalStorage('TODOS_V1', [])
   const [SearchValue, setSearchValue] = React.useState('');
 
   const completedTodos = todos.filter(todo => todo.completed).length;
@@ -43,11 +56,6 @@ function App() {
     });
   }
 
-  function saveTodos (newTodos) {
-    const stringifiedTodos = JSON.stringify(newTodos);
-    localStorage.setItem('TODOS_V1', stringifiedTodos);
-    setTodos(newTodos);
-  }
 
   const completeTodos = (text) => {
     const todoIndex = todos.findIndex(todo => todo.text === text)
