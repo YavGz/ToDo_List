6c6ace3dd814e4589cc0d425ced0f0bd52f1dd3b
Yav Gz <gutierrez.alcantara.javier@gmail.com>
2023-12-17 16:57:50 -0600

    Form buttons finished

--- src/TodoForm/index.js
+++ src/TodoForm/index.js
@@ -1,9 +1,25 @@
 import React from "react";
 import './index.css'
+import { TodoContext } from "../TodoContext";
 
 function TodoForm() {
+  const {
+    // addTodo,
+    setOpenModal,
+  } = React.useContext(TodoContext)
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    setOpenModal(false);
+  }
+
+  const onCancel = () => {
+    setOpenModal(false);
+  }
+
+
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <div>
         <label>Escribe tu nuevo TODO</label>
         <textarea
@@ -11,8 +27,19 @@ function TodoForm() {
         />
       </div>
       <div className="TodoForm-buttonContainer">
-        <button className="TodoForm-button--cancel TodoForm-button" type="button">Cancelar</button>
-        <button className="TodoForm-button--add TodoForm-button" type="button">Añadir</button>
+        <button
+          className="TodoForm-button--cancel TodoForm-button" type="button"
+          onClick={onCancel}
+        >
+          Cancelar
+        </button>
+        <button
+          className="TodoForm-button--add TodoForm-button" 
+          type="button"
+          onClick={onSubmit}
+        >
+          Añadir
+        </button>
       </div>
     </form>
   )
